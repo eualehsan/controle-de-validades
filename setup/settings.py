18089098ef8a0ba931a98447a248f5494c2f3131
alessandro <eualehsan28@gmail.com>
2025-09-18 20:25:21 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOST","127.0.0.1,localhost,eualehsan.pythonanywhere.com").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOST","").split(",")
 
 
 # Application definition
